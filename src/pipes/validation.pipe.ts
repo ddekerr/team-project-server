f4d7c091b6bf12f73348f27a1cdbe518fb251ff2
ddekerr <dimon.bond.94@gmail.com>
2023-12-04 14:09:35 +0200

    add validation pipe

---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,22 @@
+import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { ValidationException } from 'src/exceptions/validation.exception';
+
+@Injectable()
+export class GlobalValidationPipe implements PipeTransform<any> {
+  async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
+    const obj = plainToClass(metadata.metatype, value);
+    const errors = await validate(obj);
+
+    if (errors.length) {
+      const messages = errors.map((e) => {
+        return `${e.property}: ${Object.values(e.constraints).join(', ')}`;
+      });
+
+      throw new ValidationException(messages);
+    }
+
+    return value;
+  }
+}
