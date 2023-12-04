88095d3e8ab7a5817f2ca255e2b58c83b392a635
ddekerr <dimon.bond.94@gmail.com>
2023-12-04 17:03:55 +0200

    connect mongoose

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION),
+  ],
 })
 export class AppModule {}
