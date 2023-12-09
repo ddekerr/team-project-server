1db6fcbd9c14de7f1ef99c828dba5d8e219645a2
ddekerr <dimon.bond.94@gmail.com>
2023-12-09 23:24:46 +0200

    connect validation

---
+++ src/filter/exception.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { validationMessages } from 'constants/messages';
+import { ValidationException } from 'exceptions/validation.exception';
+import { Response } from 'express';
+import { ApiError } from 'helpers/ApiError';
+import { ApiValidationError } from 'helpers/ApiValidationError';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    let res = null;
+    if (exception instanceof ValidationException) {
+      res = new ApiValidationError(
+        status,
+        validationMessages.VALIDATION_ERROR,
+        exception.errors,
+      );
+    } else {
+      res = new ApiError(status, exception.message);
+    }
+
+    console.log(res);
+
+    response.status(status).json(res);
+  }
+}
