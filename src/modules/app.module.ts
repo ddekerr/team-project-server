659308fd010c8b06df89fb5401e9118baeb7075f
ddekerr <dimon.bond.94@gmail.com>
2023-12-09 19:41:57 +0200

    delete mongo connection

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_CONNECTION),
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
