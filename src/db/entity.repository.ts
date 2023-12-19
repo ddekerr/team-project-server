f923d7c472bf28fefc6d8c112dedcac0bdaf07ba
ddekerr <dimon.bond.94@gmail.com>
2023-12-19 21:34:55 +0200

    Add default repository model

---
+++ src/db/entity.repository.ts
@@ -0,0 +1,26 @@
+import { Document, FilterQuery, Model } from 'mongoose';
+
+export abstract class EntityRepository<T extends Document> {
+  constructor(protected readonly entityModel: Model<T>) {}
+
+  async findOne(
+    entityFilterQuery: FilterQuery<T>,
+    projection?: Record<string, unknown>,
+  ): Promise<T | null> {
+    return this.entityModel
+      .findOne(entityFilterQuery, {
+        _id: 0,
+        __v: 0,
+        ...projection,
+      })
+      .exec();
+  }
+
+  async find(entityFilterQuery: FilterQuery<T>): Promise<T[] | null> {
+    return this.entityModel.find(entityFilterQuery);
+  }
+
+  async create(createEntityData: unknown): Promise<T | T[]> {
+    return this.entityModel.create(createEntityData);
+  }
+}
