1b0e023f5f79647037ce45e36df05419d696ed04
ddekerr <dimon.bond.94@gmail.com>
2024-02-02 19:56:36 +0200

    Add category dtos

---
+++ src/modules/categories/dto/create-category.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength, MaxLength, IsOptional } from 'class-validator';
+import validationMessage from 'constants/validationMessage';
+
+export class CreateCategoryDto {
+  @ApiProperty()
+  @IsNotEmpty({ message: validationMessage.CATEGORY_TITLE_EMPTY_MSG })
+  @IsString({ message: validationMessage.CATEGORY_TITLE_STRING_MSG })
+  @MinLength(3, { message: validationMessage.CATEGORY_TITLE_MIN_LENGTH_MSG })
+  @MaxLength(64, { message: validationMessage.CATEGORY_TITLE_MAX_LENGTH_MSG })
+  readonly title: string;
+
+  @ApiProperty()
+  @IsNotEmpty({ message: validationMessage.CATEGORY_PARENT_EMPTY_MSG })
+  @IsString({ message: validationMessage.CATEGORY_PARENT_STRING_MSG })
+  @IsOptional()
+  readonly parent?: string;
+}
