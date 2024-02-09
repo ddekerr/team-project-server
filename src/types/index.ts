1a56bdf53c31942bb155e349d7c3e6b207996826
ddekerr <dimon.bond.94@gmail.com>
2024-02-09 11:41:38 +0200

    Improove ApiResponse

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,17 @@
 export type ValidationError = {
   [type: string]: string[];
 };
+
+export enum Actions {
+  CREATE = 'CREATE',
+  UPDATE = 'UPDATE',
+  DELETE = 'DELETE',
+  GET = 'GET',
+  ADD_POSTER = 'ADD_POSTER',
+  ADD_CATEGORY = 'ADD_CATEGORY',
+}
+
+export enum EntityType {
+  PRODUCT = 'PRODUCT',
+  CATEGORY = 'CATEGORY',
+}
