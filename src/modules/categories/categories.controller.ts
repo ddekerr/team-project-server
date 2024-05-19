8dbb40b4e65957e412ce888e65e691d8edee1193
Dima Pochupaylo <pochupaylodmitro2.0@gmail.com>
2024-05-19 10:56:17 +0300

    Documentation categori

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,21 +1,16 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiBody, ApiNotFoundResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import {ApiTags } from '@nestjs/swagger';
 
 import { CategoriesService } from './categories.service';
 import { Category, CategoryDocument } from './schemas/category.schema';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
-import exceptionMessages from 'constants/exceptionMessages';
-import validationMessage from 'constants/validationMessage';
 import { Actions, EntityType } from 'types';
 import { ResponseCategory } from './types';
 
-import { ApiSwaggerArrayResponse } from 'helpers/ApiSwaggerArrayResponse';
 import { ApiResponse } from 'helpers/ApiResponse';
-import { ApiError } from 'helpers/ApiError';
-import { ApiSwaggerResponse } from 'helpers/ApiSwaggerResponse';
-import { ApiValidationError } from 'helpers/ApiValidationError';
+import { CreateCategory, DeleteCategory, GetListCategory, UpdateCategory } from './helpers/SwaggerDocumentation';
 
 @ApiTags('Categories')
 @Controller('api/categories')
@@ -24,15 +19,7 @@ export class CategoriesController {
 
   // #################### CREATE NEW CATEGORY ####################
   @Post()
-  @ApiOperation({
-    summary: 'Create new category',
-    description:
-      'If there is a "parent" field in Request Body, then the created category is added to parent categoty in the "children" field',
-  })
-  @ApiBody({ type: CreateCategoryDto })
-  @ApiSwaggerResponse(Actions.CREATE, EntityType.CATEGORY, Category)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG })
-  @ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR })
+  @CreateCategory()
   async create(@Body() dto: CreateCategoryDto): Promise<ApiResponse<ResponseCategory>> {
     const category = await this.categoriesService.create(dto);
     return new ApiResponse(Actions.CREATE, EntityType.CATEGORY, category);
@@ -40,12 +27,7 @@ export class CategoriesController {
 
   // #################### UPDATE CATEGORY BY SLUG ####################
   @Patch(':slug')
-  @ApiOperation({ summary: 'Update one category by slug' })
-  @ApiParam({ name: 'slug', type: String })
-  @ApiBody({ type: UpdateCategoryDto })
-  @ApiSwaggerResponse(Actions.UPDATE, EntityType.CATEGORY, Category)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG })
-  @ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR })
+  @UpdateCategory()
   async update(
     @Param() { slug }: { slug: string },
     @Body() dto: UpdateCategoryDto,
@@ -56,10 +38,7 @@ export class CategoriesController {
 
   // #################### DELETE CATEGORY BY SLUG ####################
   @Delete(':slug')
-  @ApiOperation({ summary: 'Delete one category by slug' })
-  @ApiParam({ name: 'slug', type: String })
-  @ApiSwaggerResponse(Actions.DELETE, EntityType.CATEGORY, Category)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG })
+  @DeleteCategory()
   async delete(@Param() { slug }: { slug: string }): Promise<ApiResponse<string>> {
     const successMessage = await this.categoriesService.delete(slug);
     return new ApiResponse(Actions.DELETE, EntityType.CATEGORY, successMessage);
@@ -78,13 +57,7 @@ export class CategoriesController {
 
   // #################### GET CATEGORIES LIST ####################
   @Get()
-  @ApiOperation({
-    summary: 'Get list of all parent categories',
-    description:
-      'Get all category which have no parent (parent = null), but they have all children categories inside "children" field',
-  })
-  @ApiSwaggerArrayResponse(Actions.GET_LIST, EntityType.CATEGORY, Category)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG })
+  @GetListCategory()
   async getList(): Promise<ApiResponse<CategoryDocument[]>> {
     const categories = await this.categoriesService.getList();
     return new ApiResponse(Actions.GET, EntityType.CATEGORY, categories);
---
+++ src/modules/categories/helpers/SwaggerDocumentation.ts
@@ -0,0 +1,62 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiBody, ApiNotFoundResponse, ApiOperation, ApiParam } from '@nestjs/swagger';
+
+import { CreateCategoryDto } from '../dto/create-category.dto';
+import { UpdateCategoryDto } from '../dto/update-category.dto';
+
+import { Actions, EntityType } from 'types';
+import validationMessage from 'constants/validationMessage';
+import exceptionMessages from 'constants/exceptionMessages';
+import { Category } from '../schemas/category.schema';
+
+import { ApiSwaggerArrayResponse } from 'helpers/ApiSwaggerArrayResponse';
+import { ApiSwaggerResponse } from 'helpers/ApiSwaggerResponse';
+import { ApiValidationError } from 'helpers/ApiValidationError';
+import { ApiError } from 'helpers/ApiError';
+
+export function CreateCategory() {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Create new category',
+      description:
+        'If there is a "parent" field in Request Body, then the created category is added to parent categoty in the "children" field',
+    }),
+    ApiBody({ type: CreateCategoryDto }),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG }),
+    ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR }),
+    ApiSwaggerResponse(Actions.CREATE, EntityType.CATEGORY, Category),
+  );
+}
+
+export function UpdateCategory() {
+  return applyDecorators(
+    ApiOperation({ summary: 'Update one category by slug' }),
+    ApiParam({ name: 'slug', type: String }),
+    ApiBody({ type: UpdateCategoryDto }),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG }),
+    ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR }),
+    ApiSwaggerResponse(Actions.UPDATE, EntityType.CATEGORY, Category),
+  );
+}
+
+export function DeleteCategory() {
+  return applyDecorators(
+    ApiOperation({ summary: 'Delete one category by slug' }),
+    ApiParam({ name: 'slug', type: String }),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG }),
+    ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR }),
+    ApiSwaggerResponse(Actions.DELETE, EntityType.CATEGORY, Category),
+  );
+}
+
+export function GetListCategory() {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Get list of all parent categories',
+      description:
+        'Get all category which have no parent (parent = null), but they have all children categories inside "children" field',
+    }),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG }),
+    ApiSwaggerArrayResponse(Actions.GET_LIST, EntityType.CATEGORY, Category),
+  );
+}
