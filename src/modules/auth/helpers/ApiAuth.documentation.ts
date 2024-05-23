957b02f5ba4d09443672fd59a03cd1ece8813c56
ddekerr <dimon.bond.94@gmail.com>
2024-05-23 11:45:07 +0300

    fix docs

--- src/modules/auth/helpers/ApiAuth.documentation.ts
+++ src/modules/auth/helpers/ApiAuth.documentation.ts
@@ -6,6 +6,7 @@ import {
   ApiBadRequestResponse,
   ApiNotFoundResponse,
   ApiUnauthorizedResponse,
+  ApiHeader,
 } from '@nestjs/swagger';
 import exceptionMessages from 'constants/exceptionMessages';
 import validationMessage from 'constants/validationMessage';
@@ -15,6 +16,7 @@ import { ApiSwaggerResponse } from 'helpers/ApiSwaggerResponse';
 import { ApiValidationError } from 'helpers/ApiValidationError';
 
 import { CreateUserDto } from 'modules/user/dto/create-user.dto';
+import { UpdateUserDto } from 'modules/user/dto/update-user.dto';
 import { User } from 'modules/user/schemas/user.schema';
 
 import { Actions, EntityType } from 'types';
@@ -46,6 +48,7 @@ export function ApiLogoutUser() {
   return applyDecorators(
     HttpCode(200),
     ApiOperation({ summary: 'Logout user, clear cookies' }),
+    ApiHeader({ name: 'Bearer Token', description: 'Token to route access' }),
     ApiSwaggerResponse(Actions.LOGOUT, EntityType.USER, String),
     ApiUnauthorizedResponse({ description: exceptionMessages.UNAUTHORIZED_TOKEN_VALID_MSG }),
     ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_USER_MSG }),
@@ -56,6 +59,7 @@ export function ApiGetMe() {
   return applyDecorators(
     HttpCode(200),
     ApiOperation({ summary: 'Get the user information, with the access token and set cookie with refresh token' }),
+    ApiHeader({ name: 'Bearer Token', description: 'Token to route access' }),
     ApiSwaggerResponse(Actions.GET, EntityType.USER, User),
     ApiUnauthorizedResponse({ description: exceptionMessages.UNAUTHORIZED_TOKEN_VALID_MSG }),
     ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_USER_MSG }),
@@ -66,6 +70,8 @@ export function ApiUpdateMe() {
   return applyDecorators(
     HttpCode(200),
     ApiOperation({ summary: 'Update user information, with the access token and set cookie with refresh token' }),
+    ApiHeader({ name: 'Bearer Token', description: 'Token to route access' }),
+    ApiBody({ type: UpdateUserDto }),
     ApiSwaggerResponse(Actions.UPDATE, EntityType.USER, User),
     ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR }),
     ApiUnauthorizedResponse({ description: exceptionMessages.UNAUTHORIZED_TOKEN_VALID_MSG }),
