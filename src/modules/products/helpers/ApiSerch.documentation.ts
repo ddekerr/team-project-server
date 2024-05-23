52954f1962f408860319c44e9f6e8fce2d913b87
Dima Pochupaylo <pochupaylodmitro2.0@gmail.com>
2024-05-23 20:24:22 +0300

    Add product documentation

---
+++ src/modules/products/helpers/ApiSerch.documentation.ts
@@ -0,0 +1,110 @@
+import { applyDecorators, HttpCode } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiBadRequestResponse,
+  ApiParam,
+  ApiQuery,
+  ApiConsumes,
+} from '@nestjs/swagger';
+import exceptionMessages from 'constants/exceptionMessages';
+import validationMessage from 'constants/validationMessage';
+import { ApiError } from 'helpers/ApiError';
+import { ApiSwaggerResponse } from 'helpers/ApiSwaggerResponse';
+import { ApiValidationError } from 'helpers/ApiValidationError';
+import { Actions, EntityType } from 'types';
+import { CreateProductDto } from '../dto/create-product.dto';
+import { Product } from '../schemas/product.schema';
+import { FileUploadDto } from '../dto/file-upload.dto';
+import { RateDto } from '../dto/rate.dto';
+
+export function ApiCreateProduct() {
+  return applyDecorators(
+    HttpCode(201),
+    ApiOperation({ summary: 'Create new product' }),
+    ApiBody({ type: CreateProductDto }),
+    ApiSwaggerResponse(Actions.CREATE, EntityType.PRODUCT, Product),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG }),
+    ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR }),
+  );
+}
+
+export function ApiUpdateProduct() {
+  return applyDecorators(
+    HttpCode(200),
+    ApiOperation({ summary: 'Update product by ID' }),
+    ApiParam({ name: 'id', type: String }),
+    ApiBody({ type: CreateProductDto }),
+    ApiSwaggerResponse(Actions.UPDATE, EntityType.PRODUCT, Product),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG }),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG }),
+    ApiBadRequestResponse({ type: ApiError, description: exceptionMessages.MONGO_INVALID_ID }),
+    ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR }),
+  );
+}
+
+export function ApiDeleteProduct() {
+  return applyDecorators(
+    HttpCode(200),
+    ApiOperation({ summary: 'Delete product by ID' }),
+    ApiParam({ name: '_id', type: String }),
+    ApiParam({ name: 'id', type: String }),
+    ApiSwaggerResponse(Actions.DELETE, EntityType.PRODUCT, Product),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG }),
+    ApiBadRequestResponse({ type: ApiError, description: exceptionMessages.MONGO_INVALID_ID }),
+    ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR }),
+  );
+}
+
+export function ApiGetOneProduct() {
+  return applyDecorators(
+    HttpCode(200),
+    ApiOperation({ summary: 'Get one product by ID' }),
+    ApiParam({ name: 'id', type: String }),
+    ApiSwaggerResponse(Actions.GET, EntityType.PRODUCT, Product),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG }),
+    ApiBadRequestResponse({ type: ApiError, description: exceptionMessages.MONGO_INVALID_ID }),
+  );
+}
+
+export function ApiGetProductList() {
+  return applyDecorators(
+    HttpCode(200),
+    ApiOperation({ summary: 'Get Product list' }),
+    ApiQuery({ name: 'category', type: String, required: false }),
+    ApiSwaggerResponse(Actions.GET_LIST, EntityType.PRODUCT, Product),
+  );
+}
+
+export function ApiUploadPosterProduct() {
+  return applyDecorators(
+    HttpCode(200),
+    ApiOperation({ summary: 'Upload poster to product by ID' }),
+    ApiBody({ type: FileUploadDto }),
+    ApiSwaggerResponse(Actions.ADD_POSTER, EntityType.PRODUCT, Product),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG }),
+    ApiConsumes('multipart/form-data'),
+    ApiBadRequestResponse({ type: ApiError, description: exceptionMessages.MONGO_INVALID_ID }),
+  );
+}
+
+export function ApiRateProduct() {
+  return applyDecorators(
+    HttpCode(200),
+    ApiOperation({ summary: 'Rate product by ID' }),
+    ApiBody({ type: RateDto }),
+    ApiSwaggerResponse(Actions.RATE, EntityType.PRODUCT, Product),
+    ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG }),
+    ApiBadRequestResponse({ type: ApiError, description: exceptionMessages.MONGO_INVALID_ID }),
+  );
+}
+
+export function ApiSearchProduct() {
+  return applyDecorators(
+    HttpCode(200),
+    ApiOperation({ summary: 'Search product by query' }),
+    ApiQuery({ name: 'search', type: String }),
+    ApiSwaggerResponse(Actions.GET_LIST, EntityType.PRODUCT, Product),
+  );
+}
--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -10,39 +10,32 @@ import {
   Delete,
   UseInterceptors,
   UploadedFile,
-  HttpCode,
   Query,
   UsePipes,
 } from '@nestjs/common';
-import {
-  ApiBadRequestResponse,
-  ApiBody,
-  ApiConsumes,
-  ApiNotFoundResponse,
-  ApiOperation,
-  ApiParam,
-  ApiQuery,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 
 import { ProductsService } from './products.service';
-import { Product, ProductDocument } from './schemas/product.schema';
+import { ProductDocument } from './schemas/product.schema';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { FileUploadDto } from './dto/file-upload.dto';
 import { RateDto } from './dto/rate.dto';
 
-import exceptionMessages from 'constants/exceptionMessages';
-import validationMessage from 'constants/validationMessage';
 import { Actions, EntityType } from 'types';
 
-import { ApiError } from 'helpers/ApiError';
 import { ApiResponse } from 'helpers/ApiResponse';
-import { ApiValidationError } from 'helpers/ApiValidationError';
-import { ApiSwaggerResponse } from 'helpers/ApiSwaggerResponse';
-import { ApiSwaggerArrayResponse } from 'helpers/ApiSwaggerArrayResponse';
 import { Params, Rating } from './types';
 import { MongooseIdValidationPipe } from './dto/mongoID.dto';
+import {
+  ApiCreateProduct,
+  ApiDeleteProduct,
+  ApiGetOneProduct,
+  ApiGetProductList,
+  ApiRateProduct,
+  ApiSearchProduct,
+  ApiUploadPosterProduct,
+} from './helpers/ApiSerch.documentation';
+import { ApiUpdateCategory } from 'modules/categories/helpers/ApiCategories.documentation';
 // import { Types } from 'mongoose';
 
 @ApiTags('Products')
@@ -52,44 +45,33 @@ export class ProductsController {
 
   // #################### SERCH PRODUCT ####################
   @Get('search')
-  async searchProduct(@Query('search') searchQuery: string) {
-    return await this.productsService.searchProduct(searchQuery);
+  @ApiSearchProduct()
+  async searchProduct(@Query('search') searchQuery: string): Promise<ApiResponse<ProductDocument[]>> {
+    const procducts = await this.productsService.searchProduct(searchQuery);
+    return new ApiResponse(Actions.GET_LIST, EntityType.PRODUCT, procducts);
   }
 
   // #################### CREATE NEW PRODUCT ####################
   @Post()
-  @HttpCode(201)
-  @ApiOperation({ summary: 'Create new product' })
-  @ApiBody({ type: CreateProductDto })
-  @ApiSwaggerResponse(Actions.CREATE, EntityType.PRODUCT, Product)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_CATEGORY_MSG })
-  @ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR })
+  @ApiCreateProduct()
   async create(@Body() dto: CreateProductDto): Promise<ApiResponse<ProductDocument>> {
     const product = await this.productsService.create(dto);
     return new ApiResponse(Actions.CREATE, EntityType.PRODUCT, product);
   }
 
   // #################### UPDATE PRODUCT BY ID ####################
-  @Patch(':_id')
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Update product by ID' })
-  @ApiParam({ name: '_id', type: String })
-  @ApiBody({ type: CreateProductDto })
-  @ApiSwaggerResponse(Actions.UPDATE, EntityType.PRODUCT, Product)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG })
-  @ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR })
-  async update(@Param('_id') _id: string, @Body() dto: UpdateProductDto) {
+  @Patch(':id')
+  @ApiUpdateCategory()
+  @UsePipes(new MongooseIdValidationPipe())
+  async update(@Param('id') _id: string, @Body() dto: UpdateProductDto) {
     const product = await this.productsService.update(_id, dto);
     return new ApiResponse(Actions.UPDATE, EntityType.PRODUCT, product);
   }
 
   // #################### DELETE PRODUCT BY ID ####################
   @Delete(':id')
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Delete product by ID' })
-  @ApiParam({ name: 'id', type: String })
-  @ApiSwaggerResponse(Actions.DELETE, EntityType.PRODUCT, Product)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG })
+  @ApiDeleteProduct()
+  @UsePipes(new MongooseIdValidationPipe())
   async delete(@Param('id') id: string): Promise<ApiResponse<ProductDocument>> {
     const product = await this.productsService.delete(id);
     return new ApiResponse(Actions.DELETE, EntityType.PRODUCT, product);
@@ -97,11 +79,7 @@ export class ProductsController {
 
   // #################### GET ONE PRODUCT BY ID ####################
   @Get(':id')
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Get one product by ID' })
-  @ApiParam({ name: 'id', type: String })
-  @ApiSwaggerResponse(Actions.GET, EntityType.PRODUCT, Product)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG })
+  @ApiGetOneProduct()
   @UsePipes(new MongooseIdValidationPipe())
   async getOne(@Param() id: string) {
     const product = await this.productsService.getOneById(id);
@@ -110,45 +88,32 @@ export class ProductsController {
 
   // #################### GET PRODUCTS LIST ####################
   @Get()
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Get Product list' })
-  @ApiQuery({ name: 'category', type: String, required: false })
-  @ApiSwaggerArrayResponse(Actions.GET_LIST, EntityType.PRODUCT, Product)
+  @ApiGetProductList()
   async getList(@Query() params: Params): Promise<ApiResponse<ProductDocument[]>> {
     const filter = this.productsService.setFilter(params);
     const products = await this.productsService.getList(filter);
-    return new ApiResponse(Actions.GET, EntityType.PRODUCT, products);
+    return new ApiResponse(Actions.GET_LIST, EntityType.PRODUCT, products);
   }
 
   // #################### ADD POSTER TO PRODUCT ####################
-  @Patch(':_id/upload-poster')
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Upload poster to product by ID' })
-  @ApiBody({ type: FileUploadDto })
-  @ApiSwaggerResponse(Actions.ADD_POSTER, EntityType.PRODUCT, Product)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG })
-  @UseInterceptors(FileInterceptor('poster'))
-  @ApiConsumes('multipart/form-data')
+  @Patch(':id/upload-poster')
+  @ApiUploadPosterProduct()
   @UsePipes(new MongooseIdValidationPipe())
+  @UseInterceptors(FileInterceptor('poster'))
   async uploadPoster(
-    @Param('id') id: string,
+    @Param('id') _id: string,
     @UploadedFile() poster: Express.Multer.File,
   ): Promise<ApiResponse<ProductDocument>> {
-    const product = await this.productsService.uploadPoster(id, poster);
+    const product = await this.productsService.uploadPoster(_id, poster);
     return new ApiResponse(Actions.ADD_POSTER, EntityType.PRODUCT, product);
   }
 
   // #################### RATE PRODUCT ####################
-  @Patch(':_id/rate')
-  @HttpCode(200)
-  @ApiOperation({ summary: 'Rate product by ID' })
-  @ApiBody({ type: RateDto })
-  @ApiSwaggerResponse(Actions.RATE, EntityType.PRODUCT, Product)
-  @ApiNotFoundResponse({ type: ApiError, description: exceptionMessages.NOT_FOUND_PRODUCT_MSG })
-  @ApiBadRequestResponse({ type: ApiValidationError, description: validationMessage.VALIDATION_ERROR })
+  @Patch(':id/rate')
+  @ApiRateProduct()
   @UsePipes(new MongooseIdValidationPipe())
-  async rateProduct(@Param('id') id: string, @Body() dto: RateDto): Promise<ApiResponse<Rating>> {
-    const rating = await this.productsService.updateRating(id, dto.value);
+  async rateProduct(@Param('id') _id: string, @Body() dto: RateDto): Promise<ApiResponse<Rating>> {
+    const rating = await this.productsService.updateRating(_id, dto.value);
     return new ApiResponse(Actions.RATE, EntityType.PRODUCT, rating);
   }
 }
