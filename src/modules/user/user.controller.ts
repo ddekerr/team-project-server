ecf50f21fe4d113941c2f43e4b7314648f72ee06
ddekerr <dimon.bond.94@gmail.com>
2024-07-05 13:43:45 +0300

    Get users list

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiResponse } from 'helpers/ApiResponse';
 import { Actions, EntityType } from 'types';
 import { ChangeRolesDto } from './dto/change-role.dto';
 import { UserDocument } from './schemas/user.schema';
 
-@Controller('api/user')
+@Controller('api/users')
 export class UserController {
   constructor(private userService: UsersService) {}
+
+  @Get()
+  async getList(): Promise<ApiResponse<UserDocument[]>> {
+    const users = await this.userService.getList();
+    return new ApiResponse(Actions.GET_LIST, EntityType.USER, users);
+  }
 
   @Post('changeRoles')
   async changeRoles(@Body() dto: ChangeRolesDto): Promise<ApiResponse<UserDocument>> {
