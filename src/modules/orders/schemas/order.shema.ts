e72b0c03cc43438817db0e22d07cf974e0c03c08
ddekerr <dimon.bond.94@gmail.com>
2024-07-18 22:27:03 +0300

    fix order

--- src/modules/orders/schemas/order.shema.ts
+++ src/modules/orders/schemas/order.shema.ts
@@ -9,10 +9,9 @@ export type OrderDocument = Order & Document;
 
 @Schema({ versionKey: false, timestamps: true })
 export class Order {
-
   @ApiProperty({ description: 'Email adres' })
   @Prop({ type: String })
-  email:string;
+  email: string;
 
   @ApiProperty({ uniqueItems: true, required: true, description: 'Unique code of order' })
   @Prop({ type: String, unique: true, required: true })
@@ -62,6 +61,7 @@ export class Order {
           quantity: Number,
           id: Number,
           productId: String,
+          inStock: Boolean,
         },
       },
     ],
